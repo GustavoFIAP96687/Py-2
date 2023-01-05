69aa88775dff3042e3cc5c28efb931e5fa9b0278
GustavoFIAP96687 <rm96687@fiap.com.br>
2023-01-05 09:48:42 -0300

    Atualizado

---
+++ CP1/Exemplo_array.py
@@ -0,0 +1,25 @@
+numeros = []
+#numeros_altos = []
+
+for i in range(0, 5, 1):
+    x = int(input('Digite um numero: '))
+    numeros.append(x)
+ 
+   #if(numeros[i] > 10):
+        #numeros_altos.append(numeros[i])
+
+print('Os numeros que você digitou foram: ')
+
+for i in range(0, 5, 1):
+    print(numeros[i])
+
+for item  in numeros:
+    print(item)
+
+#append criar posiçao
+ 
+# Exibir de forma crescente
+#for i in range(0, 5, 1):
+    #print(numeros[i])
+
+#maior = numeros[0]
